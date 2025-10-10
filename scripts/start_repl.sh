a63bd6c40818ad8f18cba47788cbfa800e461397
Ashley Trowell <ashley.trowell@matrixresearch.com>
2025-10-10 00:20:19 -0500

    Init

---
+++ scripts/start_repl.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+# Change directory to the script location
+cd "$(dirname "${BASH_SOURCE[0]}" )"
+cd .. # Move to package root
+
+
+# Open an interactive julia session
+julia -i --project=. -e 'using Revise; using ReplTree'
